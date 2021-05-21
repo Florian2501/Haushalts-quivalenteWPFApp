f420fa0be122ac4b7a8180fcbbb072348ca3e771
Florian2501 <florischierz@gmail.com>
2021-05-21 22:53:40 +0200

    Improves the blue design
    makes the color of teh sidebar and the table header the same
    #5

--- HaushaltsäquivalenteWPFApp/Design/BlueDesign.cs
+++ HaushaltsäquivalenteWPFApp/Design/BlueDesign.cs
@@ -7,7 +7,7 @@ namespace HaushaltsäquivalenteWPFApp
 {
     class BlueDesign : Design
     {
-        public BlueDesign() : base((Color)ColorConverter.ConvertFromString("#b4daee"), (Color)ColorConverter.ConvertFromString("#19709d"), (Color)ColorConverter.ConvertFromString("#5ac7ff"), (Color)ColorConverter.ConvertFromString("#c6dbe5"), (Color)ColorConverter.ConvertFromString("#00405f"))
+        public BlueDesign() : base((Color)ColorConverter.ConvertFromString("#b4daee"), (Color)ColorConverter.ConvertFromString("#19709d"), (Color)ColorConverter.ConvertFromString("#5ac7ff"), (Color)ColorConverter.ConvertFromString("#c6dbe5"), (Color)ColorConverter.ConvertFromString("#19709d"))
         {
         }
     }
