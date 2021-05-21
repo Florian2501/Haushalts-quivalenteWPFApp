999e687978b561ce711d192bce0c7b077ce37240
Florian2501 <florischierz@gmail.com>
2021-05-21 22:39:47 +0200

    Adds the Blue design #5

---
+++ HaushaltsäquivalenteWPFApp/Design/BlueDesign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace HaushaltsäquivalenteWPFApp
+{
+    class BlueDesign : Design
+    {
+        public BlueDesign() : base((Color)ColorConverter.ConvertFromString("#b4daee"), (Color)ColorConverter.ConvertFromString("#19709d"), (Color)ColorConverter.ConvertFromString("#5ac7ff"), (Color)ColorConverter.ConvertFromString("#c6dbe5"), (Color)ColorConverter.ConvertFromString("#00405f"))
+        {
+        }
+    }
+}
