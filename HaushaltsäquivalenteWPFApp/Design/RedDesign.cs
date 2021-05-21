0a983ec256c3301047f8c1956860badafe0143ef
Florian2501 <florischierz@gmail.com>
2021-05-21 22:53:07 +0200

    Adds the red Design #5

---
+++ HaushaltsäquivalenteWPFApp/Design/RedDesign.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace HaushaltsäquivalenteWPFApp
+{
+    class RedDesign : Design
+    {
+        public RedDesign() : base((Color)ColorConverter.ConvertFromString("#ffeded"), (Color)ColorConverter.ConvertFromString("#ac2d2d"), (Color)ColorConverter.ConvertFromString("#f53f26"), (Color)ColorConverter.ConvertFromString("#ffa598"), (Color)ColorConverter.ConvertFromString("#ac2d2d"))
+        {
+        }
+    }
+}
