c5f385917ff88df4396986181b1c766d043f1edf
Florian2501 <florischierz@gmail.com>
2021-06-06 22:59:05 +0200

    Base Design of the new Person Window
    #15

---
+++ HaushaltsäquivalenteWPFApp/NewPersonWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HaushaltsäquivalenteWPFApp
+{
+    /// <summary>
+    /// Interaktionslogik für NewPerson.xaml
+    /// </summary>
+    public partial class NewPersonWindow : Window
+    {
+        public NewPersonWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the correct Design of the Window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Sets the correct colors of the ColorTheme
+            this.Background = new SolidColorBrush(ColorTheme.design.Background);
+            this.Foreground = new SolidColorBrush(ColorTheme.design.Foreground);
+            SideMenu.Background = new SolidColorBrush(ColorTheme.design.SideMenu);
+            foreach(string name in Persons.Names)
+            {
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = name;
+                GivenNames.Children.Add(textBlock);
+            }
+        }
+
+        /// <summary>
+        /// The function behind the Table Button to open the TableWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TableButton_Click(object sender, RoutedEventArgs e)
+        {
+            TableWindow tableWindow = new TableWindow();
+            tableWindow.Show();
+            this.Close();
+        }
+
+        /// <summary>
+        /// CLoses the current TableWindow and opens the Main Window again
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        /// <summary>
+        /// Opens the TaskWindow and closes this window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DoneTasksButton_Click(object sender, RoutedEventArgs e)
+        {
+            TaskWindow taskWindow = new TaskWindow();
+            taskWindow.Show();
+            this.Close();
+        }
+    
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
