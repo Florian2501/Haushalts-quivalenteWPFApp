8be7e5c20f1513d96156111e3ba92d65fc46b331
Florian2501 <florischierz@gmail.com>
2021-06-12 10:58:28 +0200

    Initializes the DetailPersonWindow.
    #21

---
+++ HaushaltsäquivalenteWPFApp/DetailPersonWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HaushaltsäquivalenteWPFApp
+{
+    /// <summary>
+    /// Interaktionslogik für DetailPersonWindow.xaml
+    /// </summary>
+    public partial class DetailPersonWindow : Window
+    {
+        public DetailPersonWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When the Table Window loads this function will be executed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowPosition.CenterWindowOnScreen(this);
+
+            //set the Color of the Background and the Menu to the Color of the Theme
+            this.Background = new SolidColorBrush(ColorTheme.design.Background);
+        }
+    }
+}
