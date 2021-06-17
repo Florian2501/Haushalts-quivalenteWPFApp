e3a250e72c5d18dbf2a634ddf1fadc3ba947b984
Florian2501 <florischierz@gmail.com>
2021-06-17 19:43:41 +0200

    IMplements the Help Window with an extended Manual
    #22

--- HaushaltsäquivalenteWPFApp/HelpWindow.xaml.cs
+++ HaushaltsäquivalenteWPFApp/HelpWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,34 @@ namespace HaushaltsäquivalenteWPFApp
         public HelpWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This function will be executed when the window loads and will center it on the screen and set the colors right.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //center the Window
+            WindowPosition.CenterWindowOnScreen(this);
+
+            //Set the Colors of the Back and Foreground
+            this.Background = new SolidColorBrush(ColorTheme.design.Background);
+            this.Foreground = new SolidColorBrush(ColorTheme.design.Foreground);
+        }
+
+        /// <summary>
+        /// This function calls the Github Page https://github.com/Florian2501/Haushalts-quivalenteWPFApp.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "https://github.com/Florian2501/Haushalts-quivalenteWPFApp";
+            psi.UseShellExecute = true;
+            System.Diagnostics.Process.Start(psi);
         }
     }
 }
